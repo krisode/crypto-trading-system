4b129d57bc10c799a5a7105746a2bcbba4d7f7e0
Tran Trinh Dang Huy <huy.ttran@katalon.com>
2024-11-20 17:45:57 +0700

    CRYPTO-004: Configure scheduler and schedule price aggregation

--- src/main/java/com/huytran/cryptotrading/cryptotradingsystem/CryptoTradingSystemApplication.java
+++ src/main/java/com/huytran/cryptotrading/cryptotradingsystem/CryptoTradingSystemApplication.java
@@ -3,9 +3,11 @@ package com.huytran.cryptotrading.cryptotradingsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class CryptoTradingSystemApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/huytran/cryptotrading/cryptotradingsystem/config/SchedulerConfig.java
@@ -0,0 +1,15 @@
+package com.huytran.cryptotrading.cryptotradingsystem.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "crypto-trading-system.config.scheduler")
+public class SchedulerConfig {
+
+    private long priceAggregationInterval;
+}
